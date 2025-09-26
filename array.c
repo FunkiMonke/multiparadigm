faa47db6dcd299ff1ec0fcd2613ee6cbf442690e
FunkiMonke <imranz.w01@gmail.com>
2025-09-26 11:34:22 +0100

    fixed the array task

--- array.c
+++ array.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 
-int display(float array[])
+int display(int l, float array[])
 {
     float avg = 0;
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < l; i++)
     {
         printf("\nEnter Competitor %d time: ", i);
         scanf("%f", &array[i]);
     }
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < l; i++)
     {
         printf("\n%d - %.2f", i, array[i]);
     }
 
-    for (int i = 0; i < sizeof(array); i++)
+    for (int i = 0; i < l; i++)
     {
         avg += array[i];
     }
-    avg = avg/sizeof(array);
-    printf("%d", avg);
+    avg = avg/l;
+    printf("\navg - %.1f", avg);
 }
 
 int main()
 {
     float time[5];
-    
-    display(time);
+    int l = sizeof(time)/sizeof(time[0]);
+    display(l, time);
     
     return 0;
 }
